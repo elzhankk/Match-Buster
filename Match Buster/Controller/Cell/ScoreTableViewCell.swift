f35eb99d1ace796fbaa418f249ea68cfb88842fa
elzhankk <elzhan-96@mail.ru>
2023-12-18 16:27:22 +0600

    Version 1.0

---
+++ Match Buster/Controller/Cell/ScoreTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ScoreTableViewCell.swift
+//  Match Buster
+//
+//  Created by elzhankk on 09.11.2023.
+//
+
+import UIKit
+
+class ScoreTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
